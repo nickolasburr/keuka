f72397236dfb37ca1f22df6d48b8b2bd598c0909
Nickolas Burr <nickolasburr@gmail.com>
2017-10-20 11:10:47 -0400

    Add note to is_error

--- src/error.c
+++ src/error.c
@@ -9,6 +9,11 @@
 /**
  * A more expressive way of determining if an
  * error was encountered from a return value.
+ *
+ * @note: This relies on the C convention of
+ *        returning -1 on error. If a function
+ *        returns a different value on error,
+ *        manually check the value instead.
  */
 int is_error (int value) {
 	return (value == -1);
