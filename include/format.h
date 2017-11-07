2684c46d0cab0f0d3a72d843828ad8ac40b1b9cd
Nickolas Burr <nickolasburr@gmail.com>
2017-11-07 14:39:19 -0500

    Add format header

---
+++ include/format.h
@@ -0,0 +1,13 @@
+/**
+ * format.h
+ *
+ * Copyright (C) 2017 Nickolas Burr <nickolasburr@gmail.com>
+ */
+
+#ifndef KEUKA_FORMAT_H
+#define KEUKA_FORMAT_H
+
+#define KEUKA_OUT_ARROW "->"
+#define KEUKA_IN_ARROW  "<-"
+
+#endif /* KEUKA_FORMAT_H */
